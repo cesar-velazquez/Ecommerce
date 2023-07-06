dc43ead257634609a986a32450830daae1acc257
cesar-velazquez <velazquezortizcesar@gmail.com>
2023-07-06 11:33:00 -0600

    cambios js y css

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,3 @@
-//inicio cambio de backgrounds en la pag
-const changeThemeHTML = document.querySelector("#changeTheme");
-changeThemeHTML.addEventListener("click", function () {
-    document.body.classList.toggle("darkmode");
-});
-
-const changeIconHTML = document.getElementById('changeTheme');
-const rectangulo = document.querySelector('.muestraropa');
-const headerSection = document.querySelector('.segundaMitad');
-const btnShowMore = document.querySelector('.show__more');
-// const btnFiltros = document.querySelectorAll('.btn__filtros');
-
-
-changeIconHTML.addEventListener('click', function () {
-    const icon = changeIconHTML.querySelector('i');
-    if (icon.classList.contains('bxs-moon')) {
-        icon.classList.remove('bxs-moon');
-        icon.classList.add('bxs-sun');
-        console.log((rectangulo.style.backgroundColor = "aqua"));        
-        console.log((btnShowMore.style.color = "#FFFFFF"));
-        console.log((btnShowMore.style.backgroundColor = "#161616"));        
-    } else {
-        icon.classList.remove('bxs-sun');
-        icon.classList.add('bxs-moon');
-        console.log((rectangulo.style.backgroundColor = "#fd135a"));
-        console.log((btnShowMore.style.color = "#FFFFFF"));
-        console.log((btnShowMore.style.backgroundColor = "#000000"));
-    }
-});
-//fin cambio de backgrounds en la pag
-
 // INICIO DEL PROCESO DE OBTENCION DE UNA API CON UNA FUNCIÓN
 const BASE_URL = "https://ecommercebackend.fundamentos-29.repl.co";
 
@@ -66,6 +35,70 @@ async function main() {
     printArticles(articles);
 }
 // FIN DEL PROCESO DE OBTENCION DE UNA API CON UNA FUNCIÓN
+
+//inicio cambio de backgrounds en la pag
+const changeThemeHTML = document.querySelector("#changeTheme");
+changeThemeHTML.addEventListener("click", function () {
+    document.body.classList.toggle("darkmode");
+});
+
+const changeIconHTML = document.getElementById('changeTheme');
+const rectangulo = document.querySelector('.muestraropa');
+const headerSection = document.querySelector('.segundaMitad');
+const btnShowMore = document.querySelector('.show__more');
+const buttonFiltros = document.querySelectorAll(".btn__products .btn__filtros");
+const footercont = document.querySelector(".section__container");
+const footertitle = document.querySelectorAll(".footer__title");
+const linkRepositorio = document.querySelector(".parrafo__a");
+const spaceSocial = document.querySelector(".footer__ul__social");
+
+changeIconHTML.addEventListener('click', function () {
+    const icon = changeIconHTML.querySelector('i');
+    if (icon.classList.contains('bxs-moon')) {
+        icon.classList.remove('bxs-moon');
+        icon.classList.add('bxs-sun');
+        console.log(icon.style.color="red");
+
+        buttonFiltros.forEach((boton) =>{
+            boton.style.backgroundColor = "#000000";
+            boton.style.color = "#ffffff";            
+        });
+        document.addEventListener("DOMContentLoaded", function() {
+            const fondotarjetas = document.querySelectorAll(".contain__product .img__prod");
+            fondotarjetas.forEach((fondo) =>{
+                fondo.style.backgroundColor = "#000039";            
+            });
+        });
+        console.log(footercont.style.backgroundColor = "#000");
+        footertitle.forEach((titulo)=>{
+            titulo.style.color = "#FFFFFF";
+        });
+        console.log(spaceSocial.style.backgroundColor="#ffffff");
+        console.log(linkRepositorio.style.color ="#000");
+        console.log((rectangulo.style.backgroundColor = "aqua"));        
+        console.log((btnShowMore.style.color = "#FFFFFF"));
+        console.log((btnShowMore.style.backgroundColor = "#161616"));        
+    } else {
+        icon.classList.remove('bxs-sun');
+        icon.classList.add('bxs-moon');    
+        buttonFiltros.forEach((boton) =>{
+            boton.style.backgroundColor = "#ffffff";
+            boton.style.color = "#000";
+        });    
+        footertitle.forEach((titulo)=>{
+            titulo.style.color = "#000";
+        });
+        console.log(spaceSocial.style.backgroundColor="#d7d1d1");
+        console.log(linkRepositorio.style.color ="#ffffff");
+        console.log(footercont.style.backgroundColor = "#ffffff");
+        console.log((rectangulo.style.backgroundColor = "#fd135a"));
+        console.log((btnShowMore.style.color = "#FFFFFF"));
+        console.log((btnShowMore.style.backgroundColor = "#000000"));
+    }
+});
+//fin cambio de backgrounds en la pag
+
+
 
 // INICIO DE UN LOADER
 function loader() {
